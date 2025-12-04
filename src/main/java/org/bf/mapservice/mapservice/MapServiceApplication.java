e75943fbfcfc6398988174b7a90324bd40afc8a6
minjakekwon <james970506@gmail.com>
2025-12-04 17:31:10 +0900

    feat: MapServiceApplication 수정 (#1)

--- src/main/java/org/bf/mapservice/mapservice/MapServiceApplication.java
+++ src/main/java/org/bf/mapservice/mapservice/MapServiceApplication.java
@@ -2,8 +2,15 @@ package org.bf.mapservice.mapservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@ComponentScan(basePackages = {
+        "org.bf.mapservice.mapservice",
+        "org.bf.global"
+})
 public class MapServiceApplication {
 
 	public static void main(String[] args) {
