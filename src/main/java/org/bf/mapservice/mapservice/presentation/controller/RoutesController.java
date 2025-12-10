ef5c97b0d4d37697732fbf20528a5c0c53b86c94
minjakekwon <james970506@gmail.com>
2025-12-10 11:17:52 +0900

    feat: 컨트롤러 응답값 변경 (#9)

--- src/main/java/org/bf/mapservice/mapservice/presentation/controller/RoutesController.java
+++ src/main/java/org/bf/mapservice/mapservice/presentation/controller/RoutesController.java
@@ -1,12 +1,12 @@
 package org.bf.mapservice.mapservice.presentation.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.bf.global.infrastructure.CustomResponse;
 import org.bf.mapservice.mapservice.application.query.FindRouteQuery;
 import org.bf.mapservice.mapservice.application.service.RouteApplicationService;
+import org.bf.mapservice.mapservice.presentation.controller.dto.RouteGeoJsonResponseDto;
 import org.bf.mapservice.mapservice.presentation.controller.dto.RoutePointDto;
 import org.bf.mapservice.mapservice.presentation.controller.dto.RouteRequestDto;
-import org.bf.mapservice.mapservice.presentation.controller.dto.RouteResponseDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +20,7 @@ public class RoutesController {
     private final RouteApplicationService routeApplicationService;
 
     @PostMapping
-    public ResponseEntity<RouteResponseDto> findRoute(@RequestBody RouteRequestDto request) {
+    public CustomResponse<RouteGeoJsonResponseDto> findRoute(@RequestBody RouteRequestDto request) {
         FindRouteQuery query = new FindRouteQuery(
                 request.startLatitude(),
                 request.startLongitude(),
@@ -28,8 +28,10 @@ public class RoutesController {
                 request.endLongitude(),
                 request.mobilityType()
         );
-
+        // 1) 서비스에서 (lat, lon) 리스트 받아오고
         List<RoutePointDto> points = routeApplicationService.findRoute(query);
-        return ResponseEntity.ok(new RouteResponseDto(points));
+        // 2) GeoJSON LineString으로 감싸서 응답
+        RouteGeoJsonResponseDto response = new RouteGeoJsonResponseDto(points);
+        return CustomResponse.onSuccess(response);
     }
 }
