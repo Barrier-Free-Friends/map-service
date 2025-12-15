12d074823b832e6bb4c39a0b0c48a2c6c3e58041
minjakekwon <james970506@gmail.com>
2025-12-15 23:27:16 +0900

    feat: 엔티티 추가 (#12)

---
+++ src/main/java/org/bf/mapservice/mapservice/domain/entity/ObstacleStatus.java
@@ -0,0 +1,5 @@
+package org.bf.mapservice.mapservice.domain.entity;
+
+public enum ObstacleStatus {
+    ACTIVE, RESOLVED, EXPIRED, DISMISSED
+}
---
+++ src/main/java/org/bf/mapservice/mapservice/domain/entity/ObstacleType.java
@@ -0,0 +1,11 @@
+package org.bf.mapservice.mapservice.domain.entity;
+
+public enum ObstacleType {
+    STAIRS,
+    CONSTRUCTION,
+    SIDEWALK_BLOCKED,
+    ROAD_BLOCKED,
+    SLOPE,
+    ELEVATOR_OUTAGE,
+    OTHER
+}
